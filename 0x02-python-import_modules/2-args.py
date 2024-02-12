675d2bcbb398e6f80aec27594b95c1de29013bef
MinaEhab77 <fouadattia09@gmail.com>
2024-02-12 08:43:06 -0800

    updated

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    length = len(sys.argv - 1)
+
+    if count == 0:
+        print("0 arguments.")
+
+    elif count == 1:
+        print("1 argument:")
